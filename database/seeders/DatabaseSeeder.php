bf618066679284373fd34fbb837bed902489257e
BenoitLecoeuvre <96916573+BenoitLecoeuvre@users.noreply.github.com>
2022-04-12 17:53:51 +0200

    Création migration "contact" et seeder lié

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
+use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,6 +16,17 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        $user = \App\Models\User::factory()->create();
+
+    DB::table('contacts')->insert([
+        'name' => Str::random(5),
+        'first-name' => Str::random(7),
+        'firm' => Str::random(3),
+        'mail' => Str::random(6).'@gmail.com',
+        'phone' => '0'.rand(6, 7).'.'.rand(010, 99).'.'.rand(10, 99).'.'.rand(10, 99).'.'.rand(10, 99),
+        'message' => Str::random(25),
+        'created_at' => now(),
+        'updated_at' => now(),
+    ]);
     }
 }

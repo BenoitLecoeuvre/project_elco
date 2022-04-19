8911884676cde1a909ec3aadefb8b3b1a5f94cdc
Nihadl99 <nihadlazaar@gmail.com>
2022-04-19 13:44:23 +0200

    page services

--- resources/views/services.blade.php
+++ resources/views/services.blade.php
@@ -15,6 +15,80 @@
           <img src="./img/titre.jpg">
       </div>
   </section>
+
+  <section>
+      
+          <div class="titre_services" >
+              <h2>
+                 Concrètement nous réalisons
+              </h2>
+
+          </div>
+       <div class="titre_services_parent">
+           <div class="titre_services" >
+               <h3>
+                 La tenue de comptabilité
+              </h3>
+               <p>
+                 Achats, ventes, immobilisations, etc.
+                 Intégration automatique des opérations bancaires et des écritures de paie (intermittents et permanents).
+               </p>
+          </div>
+      
+
+          <div class="titre_services" >
+               <h3>
+                 L'établissement des déclarations fiscales
+              </h3>
+               <p>
+                TVA, Impôts sur les sociétés,
+                liasse fiscale, etc.
+                Télédéclaration et envoi des autorisations de prélèvement à l’administration fiscale.
+               </p>
+          </div>
+      
+         
+          <div class="titre_services" >
+               <h3>
+                 La révision et l'établissement
+                 des comptes annuels
+              </h3>
+               <p>
+                Les comptes annuels sont composés d’un ensemble de 3 documents : le bilan comptable, le compte de résultat et l’annexe légale.
+              </p>
+          </div>
+      </div>
+
+      <div class="titre_service_parent_">
+          <div class="titre_services" >
+               <h3>
+                 La présentation détaillée du bilan et
+                 du compte résultat
+               </h3>
+               <p>
+                 Présentation des éléments relatifs aux actifs et aux passifs de l’entreprise ainsi que l’ensemble des charges et des produits de l’exercice.
+               </p>
+          </div>
+     
+           <div class="titre_services" >
+               <h3>
+                 La comptabilité analytique par projet
+              </h3>
+               <p>
+                 Suivi des dépenses de production par projet en temps réel.
+               </p>
+          </div>
+       
+           <div class="titre_services" >
+              <h3>
+                 Des conseils et accompagnements
+              </h3>
+               <p>
+                 Vous accompagner au quotidien dans vos démarches et répondre à vos questions est la mission de notre équipe d’experts-comptables.
+               </p>
+           </div>
+        </div> 
+    </section>
 </main>
     
 @endsection

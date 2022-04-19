23c0acfdb9471cc0b7d55cc2f2c5d06334aa8ab0
Nihadl99 <nihadlazaar@gmail.com>
2022-04-19 10:32:33 +0200

    création page qui sommes nous

--- resources/views/presentations.blade.php
+++ resources/views/presentations.blade.php
@@ -18,58 +18,78 @@
   </section>
 </main>
 <article id="presentation">
-  <div class="titre_pre">
-    <h2>
-    Conseil en comptabilité d'entreprise à Arras avec les experts de l'A.R.Ge.Co
-    </h2>
+
+  <div class="premier_pre">
+    <div class="titre_pre">
+      <h2>
+        Conseil en comptabilité d'entreprise à Arras avec les experts de l'A.R.Ge.Co
+      </h2>
+    <div class="paragraphe1">
+      <p>
+        Nos plus de 40 ans d’expérience et nos compétences multiples en expertise comptable, en gestion sociale, 
+        juridique et en conseil et gestion d’entreprise nous permettent d’accompagner les professionnels issus de nombreux secteurs d’activité : 
+        artisanat, commerces, professions libérales, associations, des TPE et TPME… Au fil du temps, nous n’avons eu de cesse de nous développer en nous répartissant sur 24 agences 
+        au sein des Hauts-de-France et du Rhône pour être au plus proche de vous !
+        Tous les ans, de nouveaux adhérents font appel à l'A.R.Ge.Co pour être accompagnés dans tous les aspects qu’implique la gestion d’entreprise : 
+        comptabilité, tâches administratives, juridiques…
+      </p>
+   </div>
+  </div> 
   
 
-  <p>
-    Nos plus de 40 ans d’expérience et nos compétences multiples en expertise comptable, en gestion sociale, 
-    juridique et en conseil et gestion d’entreprise nous permettent d’accompagner les professionnels issus de nombreux secteurs d’activité : 
-    artisanat, commerces, professions libérales, associations, des TPE et TPME… Au fil du temps, nous n’avons eu de cesse de nous développer en nous répartissant sur 24 agences 
-    au sein des Hauts-de-France et du Rhône pour être au plus proche de vous !
-    Tous les ans, de nouveaux adhérents font appel à l'A.R.Ge.Co pour être accompagnés dans tous les aspects qu’implique la gestion d’entreprise : 
-    comptabilité, tâches administratives, juridiques…
-  </p>
-  <div class="image_pre">
-    <img src="./img/comptabilité_pre.jpeg" alt="">
+  <div class="deuxieme_pre">
+   <div class="image_pre1">
+     <img src="./img/comptabilité_pre.jpeg" alt="">
+   </div>
+   <h3>
+     Les valeurs qui font toute notre force
+   </h3>
+
+   <div class="paragraphe2">
+    <p>
+      Les valeurs qui font toute notre force
+      L'association A.R.Ge.Co met ses compétences en comptabilité et son expérience de conseil en gestion d’entreprise au service de vos projets de développement. 
+      Depuis notre création, nous avons mis en place une organisation nous permettant de vous accompagner à chaque instant ! C’est avec pour valeurs le respect, 
+      l’écoute, la disponibilité et la confiance que nous nous engageons à vos côtés.
+    </p>
+   </div>
   </div>
-  <h3>
-    Les valeurs qui font toute notre force
-  </h3>
-  <p>
-    Les valeurs qui font toute notre force
-    L'association A.R.Ge.Co met ses compétences en comptabilité et son expérience de conseil en gestion d’entreprise au service de vos projets de développement. 
-    Depuis notre création, nous avons mis en place une organisation nous permettant de vous accompagner à chaque instant ! C’est avec pour valeurs le respect, 
-    l’écoute, la disponibilité et la confiance que nous nous engageons à vos côtés.
-  </p>
-  <div class="image_pre_gauche">
-    <img src="./img/image_pre2.jpeg" alt="">
-  </div>
-  <h3>
-    Avec l'A.R.Ge.Co, vous avez tout à gagner !
-  </h3>
-  <div class="text_pre_droite">
-  <p>
-    Avec l'A.R.Ge.Co, vous avez tout à gagner ! 
-    Avec les comptables professionnels de l'A.R.Ge.Co, vous bénéficiez d’un accompagnement et de services spécialement conçus pour améliorer la performance de votre société. 
-    Vous êtes accompagné par un conseiller unique maîtrisant totalement votre secteur d’activité et les spécificités de votre métier. L'A.R.Ge.Co, 
-    c’est aussi des experts dans chaque domaine à votre service : création d’entreprise, gestion sociale, juridique, gestion d'entreprise… De plus, avec notre dimension associative, 
-    vous êtes au cœur de toutes nos attentions.
-  </p>
-  </div>
-  <div class="image_pre">
-    <img src="./img/image_pre3.jpeg" alt="">
-  </div>
-  <h3>
-    Pourquoi vous faire assister par l'A.R.Ge.Co pour votre comptabilité ?
-  </h3>
-  <p>
-    À l'A.R.Ge.Co, nous vous accompagnons à chaque instant. Contrôle social ou fiscal, consolidation de votre bilan, améliorer vos résultats ou encore prévisionner votre trésorerie… 
-    Vous pouvez vous reposer sur nos connaissances parfaites de votre secteur d’activité. Artisans, commerçants, professions libérales, 
-    associations, TPE, PME…, en nous confiant la gestion de votre comptabilité, vous faites le choix de bénéficier d’un accompagnement personnalisé.
-  </p>
-</div>
+
+
+  <div class="troisieme_pre">
+    <div class="image_pre_gauche">
+      <img src="./img/image_pre2.jpeg" alt="">
+   </div>
+     <h3>
+       Avec l'A.R.Ge.Co, vous avez tout à gagner !
+     </h3>
+   <div class="paragraphe3">
+      <p>
+        Avec l'A.R.Ge.Co, vous avez tout à gagner ! 
+        Avec les comptables professionnels de l'A.R.Ge.Co, vous bénéficiez d’un accompagnement et de services spécialement conçus pour améliorer la performance de votre société. 
+        Vous êtes accompagné par un conseiller unique maîtrisant totalement votre secteur d’activité et les spécificités de votre métier. L'A.R.Ge.Co, 
+        c’est aussi des experts dans chaque domaine à votre service : création d’entreprise, gestion sociale, juridique, gestion d'entreprise… De plus, avec notre dimension associative, 
+        vous êtes au cœur de toutes nos attentions.
+     </p>
+   </div>
+ </div>
+
+
+ <div class="quatrieme_pre">
+   <div class="image_pre2">
+     <img src="./img/image_pre3.jpeg" alt="">
+   </div>
+    <h3>
+      Pourquoi vous faire assister par l'A.R.Ge.Co pour votre comptabilité ?
+   </h3>
+    <div class="paragraphe1">
+      <p>
+        À l'A.R.Ge.Co, nous vous accompagnons à chaque instant. Contrôle social ou fiscal, consolidation de votre bilan, améliorer vos résultats ou encore prévisionner votre trésorerie… 
+        Vous pouvez vous reposer sur nos connaissances parfaites de votre secteur d’activité. Artisans, commerçants, professions libérales, 
+        associations, TPE, PME…, en nous confiant la gestion de votre comptabilité, vous faites le choix de bénéficier d’un accompagnement personnalisé.
+     </p>
+   </div>
+ </div>
+  
 </article>
 @endsection

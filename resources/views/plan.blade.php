12a9684885acad7ec786abd3b4c103de2f949c3f
Nihadl99 <nihadlazaar@gmail.com>
2022-05-03 17:53:56 +0200

    plan du site

--- resources/views/plan.blade.php
+++ resources/views/plan.blade.php
@@ -2,4 +2,25 @@
 
 @section('content')
 
+<main>
+   
+   
+   
+    
+    <div class="Plan_div1">
+        <a href="">Accueil</a>
+        <a href="">Qui sommes-nous ?</a>
+        <a href="">Espace Client</a>
+    </div>
+ 
+    <hr width="50%" color="#ba7ed5">
+
+    <div class="Plan_div2">
+        <a href="">Nos services</a>
+        <a href="">Mention légal</a>
+        <a href="">Contact</a>
+    </div>
+</main>
+
+
 @endsection

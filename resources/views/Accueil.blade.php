ca3d8483623ca4a8cfd313780817d79128fffed7
Nihadl99 <nihadlazaar@gmail.com>
2022-05-05 14:34:04 +0200

    carrousel

--- resources/views/Accueil.blade.php
+++ resources/views/Accueil.blade.php
@@ -3,15 +3,18 @@
 @section('content')
 <section class="main">
 
-   <h1>Le beau slider</h1>
-   <div id="slider">
-       <img src="{{url('img/elco-3.png')}}" id="slide">
-       <div id="precedent" onclick="ChangeSlide(-1)"><</div>
-       <div id="suivant" onclick="ChangeSlide(1)">></div>
-   </div>
+   <div class="container_slide">
+      <div class="slider">
+        <img class="active" src="img/elco-3.png" alt="">
+        <img src="img/image_compta.png">
+        <img src="img/image_pre2.jpeg">
+      </div>
    
-
-
+      <div class="cont-btn">
+         <div class="btn-nav left">←</div>
+         <div class="btn-nav right">→</div>
+      </div>
+</div>
 
    <div class="intro">
       <h2>Depuis plus de 20 ans, les experts-comptables d'ELCO vous proposent leurs services et prestations</h2>
@@ -44,19 +47,55 @@
       </div>
    </div>
 
-<script> 
-   var slide = new Array("elco-3.png","image_compta.png", "image_pre3.jpeg","image_pre2.jpeg");
-var numero = 0;
 
-function ChangeSlide(sens) {
-    numero = numero + sens;
-    if (numero < 0)
-        numero = slide.length - 1;
-    if (numero > slide.length - 1)
-        numero = 0;
-    document.getElementById("slide").src = slide[numero];
+<script> 
+ const items = document.querySelectorAll('img');
+const nbSlide = items.length;
+const suivant = document.querySelector('.right');
+const precedent = document.querySelector('.left');
+let count = 0;
+
+function slideSuivante(){
+    items[count].classList.remove('active');
+
+    if(count < nbSlide - 1){
+        count++;
+    } else {
+        count = 0;
+    }
+
+    items[count].classList.add('active')
+    console.log(count);
+    
 }
-   
+suivant.addEventListener('click', slideSuivante)
+
+
+function slidePrecedente(){
+    items[count].classList.remove('active');
+
+    if(count > 0){
+        count--;
+    } else {
+        count = nbSlide - 1;
+    }
+
+    items[count].classList.add('active')
+    // console.log(count);
+    
+}
+precedent.addEventListener('click', slidePrecedente)
+
+function keyPress(e){
+    console.log(e);
+    
+    if(e.keyCode === 37){
+        slidePrecedente();
+    } else if(e.keyCode === 39){
+        slideSuivante();
+    }
+}
+document.addEventListener('keydown', keyPress)
 </script>
 </section>
        
